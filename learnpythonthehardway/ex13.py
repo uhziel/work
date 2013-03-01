3b2b0adc4955bafb2bd4201508b7e108697e6d78
Lei Zhu <uhziel@gmail.com>
2013-03-01 23:43:45 +0800

    添加处理 sys.argv 的。

---
+++ learnpythonthehardway/ex13.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python2
+
+from sys import argv
+
+var0, var1, var2, var3 = argv
+
+print "var0 = %s" % var0
+print "var1 = %s" % var1
+print "var2 = %s" % var2
+print "var3 = %s" % var3
